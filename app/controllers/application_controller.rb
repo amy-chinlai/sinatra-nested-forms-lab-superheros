d5b0e9091c94837fc02c04ae95651534ea337cca
amy-chinlai <amy@actionnetwork.org>
2021-09-09 14:10:37 -0400

    commit tree 465a11e32b20f7621c5fdc37e42a757094a91cb0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
 
+    post '/teams' do
+        @team = Team.new(params[:team][:name], params[:team][:motto])
+        params[:team][:members].each do |m|
+            SuperHero.new(m[:name], m[:power], m[:bio])
+        end
+        @members = SuperHero.all
+        erb :team
+    end
 end
